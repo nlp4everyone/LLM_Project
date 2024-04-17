cf55caf2e97250ac66bff8c23adf5201b2b21239
johnstone343 <jone.stones34xyz@outlook.com>
2024-04-17 19:01:19 +0700

    update semantic splitter

--- ingestion_modules/text_splitter/custom_splitter.py
+++ ingestion_modules/text_splitter/custom_splitter.py
@@ -2,7 +2,8 @@ from semantic_text_splitter import TextSplitter
 from typing import Union,List,Tuple
 from enum import Enum
 from tokenizers import Tokenizer
-
+from llama_index.core import Document
+from llama_index.core.node_parser import NodeParser
 
 class Splitter_Type(Enum):
     CHARACTER_MODE = 0,
@@ -12,6 +13,7 @@ class Splitter_Type(Enum):
 class SemanticTextSplitter():
     # Reference (https://pypi.org/project/semantic-text-splitter/)
     def __init__(self,splitter_mode = Splitter_Type.CHARACTER_MODE):
+        super().__init__()
         # Base mode
         self.splitter = TextSplitter()
         if splitter_mode == Splitter_Type.HUGGINGFACE_MODE:
@@ -24,3 +26,11 @@ class SemanticTextSplitter():
         if not type(input) == str:
             raise Exception("Please insert list of string")
         return self.splitter.chunks(input,max_characters)
+
+    def from_documents(self,documents: List[Document],max_characters : Union[int,Tuple[int]]= (100,1000)):
+        # Join document
+        text = [doc.text for doc in documents]
+        text = "\n".join(text)
+
+        # Return value
+        return self.splitter.chunks(text, max_characters)
--- test.py
+++ test.py
@@ -16,8 +16,7 @@
 # agent = ReActAgent.from_tools([multiply_tool], llm=llm, verbose=True)
 # output =  agent.query("1 multiple 2")
 # print(output)
-
-
+import os
 
 # from ai_modules.llm_modules.service_llm import ServiceChatModel
 # # from ai_modules.llm_modules.open_llm import OllamaChatModel
@@ -54,4 +53,49 @@
 #     # model_kwargs={"torch_dtype": torch.float16}
 # )
 # print(dir(llm))
+
+# from llama_index.core import Document
+# doc1 = Document(text="Whales Market is a unique trading platform designed to make swapping assets across different blockchains easy and secure. It tackles the common problem of scams and fraud in peer-to-peer (P2P) cryptocurrency trading of pre-launch tokens and points before TGE.",metadata={"category":"Whales Market","type":"definition"})
+# doc2 = Document(text="Current price of Whales is 3$",metadata={"category":"Whales Market","type":"price"})
+# doc3 = Document(text="Neymar da Silva Santos Júnior (born 5 February 1992), also known as Neymar Júnior, is a Brazilian professional footballer who plays as a forward for Saudi Pro League club Al Hilal and the Brazil national team",metadata={"category":"Neymar","type":"definition"})
+#
+# from ai_modules.chatmodel_modules.service_chatmodel import ServiceChatModel
+# from config import params
+# os.environ["OPENAI_API_KEY"] = params.OPENAI_KEY
+# print(params.OPENAI_KEY)
+# # service_chatmodel = ServiceChatModel(service_name="GEMINI")
+# # chat_model = service_chatmodel.get_chat_model()
+# doc = [doc1,doc2,doc3]
+# from llama_index.core import VectorStoreIndex
+# index = VectorStoreIndex.from_documents(doc)
+# query_engine = index.as_query_engine()
+# ans = query_engine.query("Who is Neymar?")
+# print(ans)
+
+from ingestion_modules.custom_loader.custom_web_loader import CustomWebLoader,WebProvider
+# Load data
+web_loader = CustomWebLoader(web_provider=WebProvider.TRAFILATURA)
+docs = web_loader.load_data("https://en.wikipedia.org/wiki/Neymar")
+from llama_index.embeddings.openai import OpenAIEmbedding
+
+from llama_index.core.node_parser import SemanticSplitterNodeParser,SentenceSplitter
+from llama_index.core.extractors import TitleExtractor
+from ingestion_modules.text_splitter.custom_splitter import SemanticTextSplitter
+# embedding = OpenAIEmbedding()
+# Ingestion
+from llama_index.core.ingestion import IngestionPipeline
+# semantic_splitter = SemanticTextSplitter()
+# semantic_docs = semantic_splitter.from_documents(docs)
+# for (i,doc) in enumerate(semantic_docs):
+#     print(f"Document {i}:")
+#     if i < 5: print(doc)
+
+pipeline = IngestionPipeline(
+    transformations=[
+        SentenceSplitter(chunk_size=1000, chunk_overlap=200),
+    ],
+)
+docs = pipeline.run(documents=docs)
+
+
 
