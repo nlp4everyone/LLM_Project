50cbffb3970d224d0ce8c3b1a20a3f658d3dc937
nlp4everyone <jone.stones34xyz@outlook.com>
2024-05-07 14:31:56 +0700

    revise pdf loader

--- ingestion_modules/loader/pdf_loader.py
+++ ingestion_modules/loader/pdf_loader.py
@@ -1,13 +1,13 @@
 from llama_index.readers.file import pymu_pdf,unstructured
 from llama_parse.base import LlamaParse
-from enum import Enum
-from config.params import *
+from config import params
 from typing import Union,List,Literal
 from pathlib import Path
 from system_component.system_logging import Logger
 import os
+LLAMAPARSE_KEY = params.LLAMAPARSE_KEY
 
-class CustomPDFReader():
+class PDFReader():
     def __init__(self,pdf_provider : Literal["PyMuPDF","Unstructured","LlamaParse"] = "PyMuPDF"):
         # Define variable
         self._pdf_provider = pdf_provider
@@ -23,6 +23,7 @@ class CustomPDFReader():
             self._reader = unstructured.UnstructuredReader()
         # LlamaParse Reader
         elif self._pdf_provider == "LlamaParse":
+            assert LLAMAPARSE_KEY, "LlamaParser key cant be empty"
             self._reader = LlamaParse(api_key=LLAMAPARSE_KEY)
         else:
             raise Exception(f"Service {self._pdf_provider} is not supported!")
