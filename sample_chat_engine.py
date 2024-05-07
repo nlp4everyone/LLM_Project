5204f496bdc31be983e7dd0fbdf9a786a9991a3e
nlp4everyone <jone.stones34xyz@outlook.com>
2024-05-07 18:54:36 +0700

    update chat engine

--- sample_chat_engine.py
+++ sample_chat_engine.py
@@ -8,6 +8,14 @@ qdrant_service = data_ingestion.qdrant_service
 llm = data_ingestion.llm
 embedding_model = data_ingestion.embedding_model
 
-def chat_response():
+def chat_response(question: str):
     # Define index
-    index = qdrant_service.load_index()
+    index = qdrant_service.load_index(embedding_model=embedding_model)
+
+    chat_engine = index.as_chat_engine(llm=llm,chat_mode="simple",verbose=True)
+    answer = chat_engine.chat(question)
+    return answer
+    
+answer = chat_response("Who is Neymar?")
+print(answer)
+
