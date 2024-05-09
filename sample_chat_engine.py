8cf047421b4d0b798365259c9cf9b2983ca57644
huannn <thesunshine36@gmail.com>
2024-05-09 10:45:08 +0700

    update package name

--- sample_chat_engine.py
+++ sample_chat_engine.py
@@ -1,12 +1,12 @@
 from ingestion_modules.custom_vectorstore.qdrant_service import QdrantService,_QDRANT_COLLECTION
 from system_component.system_logging import Logger
 from ai_modules.query_modules.custom_query_engine import BaseQueryEngine
-import data_ingestion
+import data_ingestion_web
 
 # Reference service
-qdrant_service = data_ingestion.qdrant_service
-llm = data_ingestion.llm
-embedding_model = data_ingestion.embedding_model
+qdrant_service = data_ingestion_web.qdrant_service
+llm = data_ingestion_web.llm
+embedding_model = data_ingestion_web.embedding_model
 
 def chat_response():
     # Define index
